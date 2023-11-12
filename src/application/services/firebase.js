60424475164ff088c749e8c93b5f678ab84748a4
thekingincatsle <dat.dt200133@sis.hust.edu.vn>
2023-11-12 22:40:20 +0700

    hide credential

--- src/application/services/firebase.js
+++ src/application/services/firebase.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/database';
-import { getDatabase } from 'firebase/database';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import { getDatabase } from "firebase/database";
 
 const config = {
-    apiKey: 'AIzaSyCFDtilLLCLJ2SKTC6U1gavJsMnYUlg7Us',
-    authDomain: 'learn-fb-a116f.firebaseapp.com',
-    projectId: 'learn-fb-a116f',
-    storageBucket: 'learn-fb-a116f.appspot.com',
-    messagingSenderId: '920113807228',
-    appId: '1:920113807228:web:f2ef6ed3dfe39913e4aef9',
-    measurementId: 'G-MLP04WT8X1',
-    databaseURL: 'https://learn-fb-a116f-default-rtdb.asia-southeast1.firebasedatabase.app',
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
+    databaseURL: process.env.databaseURL,
 };
 
 const app = firebase.initializeApp(config);
